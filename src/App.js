2aa53a66ae168626e9f0ca5cd3c4c63437db109c
ifoster01 <ifoster41901@gmail.com>
2022-11-02 12:36:14 -0400

    updated App.js
    now includes homebody

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import Header from "./Components/Header"
 import NavBar from "./Components/NavBar"
-import HomeBody from "./Components/HomeBody"
+// import HomeBody from "./Components/HomeBody"
+import './Components/HomeBody.css'
 import About from "./Components/About"
 import ClassHome from "./Components/ClassHome"
 import ClassResults from "./Components/ClassResults"
@@ -13,7 +14,7 @@ import BPLLogo from "./Images/logo.png"
 import GitHubLogo from "./Images/github-logo.png"
 import RMPData from './Data/rmp.json'
 import CourseData from './Data/courses.json'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useResolvedPath, useMatch, Link } from 'react-router-dom'
 
 function App() {
   // for class search data structure:
@@ -60,6 +61,26 @@ function App() {
         </Routes>
       </div>
       <Footer gitLogo={GitHubLogo}/>
+    </div>
+  )
+}
+
+// to reflect if the page is currently active as well as setting the id,
+// redirect address and displayed information
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path : resolvedPath.pathname, end: true })
+
+  return <Link id='search-btn' className={isActive ? "active" : ""} to={to} {...props}>{children}</Link>
+}
+
+function HomeBody() {
+  return (
+    <div>
+      <input className="professor-search" type="text" placeholder="Search Professors...." />
+      <CustomLink to="/classresults">Search</CustomLink>
+      <input className="class-search" type="text" placeholder="Search Classes...." />
+      <CustomLink to="/profresults">Search</CustomLink>
     </div>
   )
 }
