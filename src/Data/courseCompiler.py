2d879df6586c98ed266f6db96b8fbf74533c8e21
Yotham <yothamsage@gmail.com>
2022-11-02 14:46:02 -0400

    revert revert

--- src/Data/courseCompiler.py
+++ src/Data/courseCompiler.py
@@ -1,7 +1,7 @@
 import json
 
 course = "csci1100"
-overall_list = []
+overall_dict = {}
 with open('courses.json') as data_file:    
     data = json.load(data_file)
     for i in range(len(data)):
@@ -11,7 +11,6 @@ with open('courses.json') as data_file:
             instructors = set()
             number= str(data[i]['courses'][j]["crse"])
             course = code+number
-            individual_dict = {}
             for k in range(len(data[i]['courses'][j]['sections'])):
                 for o in range(len(data[i]['courses'][j]['sections'][k]['timeslots'])):
                     instructor = str(data[i]['courses'][j]['sections'][k]['timeslots'][o]['instructor'])
@@ -23,11 +22,8 @@ with open('courses.json') as data_file:
                             instructors.add(instructor)
             if(instructors):
                 instructors = list(instructors)
-                individual_dict["course"] = course
-                individual_dict["list"] = instructors
-                overall_list.append(individual_dict)
-            
+                overall_dict[course] = instructors
 
-print(overall_list)
+
 with open("courseProfs.json", "w") as outfile:
-    json.dump(overall_list, outfile,indent = 4)
+    json.dump(overall_dict, outfile,indent = 4)
