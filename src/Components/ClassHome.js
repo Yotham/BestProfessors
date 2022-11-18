30db30e3ed0393a28b32ad6711f8c47b39a2a34f
ifoster01 <ifoster41901@gmail.com>
2022-11-18 17:03:56 -0500

    added search to classes home

--- src/Components/ClassHome.js
+++ src/Components/ClassHome.js
@@ -1,49 +1,64 @@
-import React from 'react'
+import React, { useState } from "react"
 import './ClassHome.css'
 
 export default function ClassHome({ data }) {
+  const [searchText, setText] = useState('');
+  const handleChange = event => {
+    setText(event.target.value.toUpperCase());
+    console.log('value is:', searchText);
+  }
+
   return (
-    <div className='course-cataloge'>
-      {
-        data && data.map( courseCode => {
-          return (
-            <div className='class-home'>
-              <div key={ courseCode.code }>
-                <div id="code">
-                  { courseCode.code }
-                </div>
-                { courseCode.courses && courseCode.courses.map( availableCourses => {
-                  return (
-                    <div id='course' key={ availableCourses.id }>
-                      { availableCourses.id } -- { availableCourses.title }
-                      { availableCourses.sections && availableCourses.sections.map( courseSections => {
-                        return (
-                          <div key={ courseSections.crn }>
-                            { courseSections.timeslots && courseSections.timeslots.map( courseInstructor => {
-                              return (
-                                <div key={ courseSections.crn }>
-                                  <span>Professor: </span>{ courseInstructor.instructor }
-                                  <br></br>
-                                </div>
-                              )
-                            }) }
-                            <span>CRN: </span>{ courseSections.crn }
-                            <br></br>
-                            <span>Seats Available: </span>{ courseSections.rem }<span>/</span>{ courseSections.cap }
-                            <br></br>
-                          </div>
-                        )
-                      }) }
-                      <br></br>
-                    </div>
-                  )
-                }) }
+    <div>
+      <input id="class-search-home" onChange = {handleChange} type="text" placeholder="Search by course code...." />
+      <div className='course-cataloge'>
+        {
+          data && data.map( courseCode => {
+            return (
+              <div className='class-home'>
+                {(() => {
+                  if (searchText === "" || courseCode.code === searchText) {
+                    return (
+                      <div key={ courseCode.code }>
+                        <div id="code">
+                          { courseCode.code }
+                        </div>
+                        { courseCode.courses && courseCode.courses.map( availableCourses => {
+                          return (
+                            <div id='course' key={ availableCourses.id }>
+                              { availableCourses.id } -- { availableCourses.title }
+                              { availableCourses.sections && availableCourses.sections.map( courseSections => {
+                                return (
+                                  <div key={ courseSections.crn }>
+                                    { courseSections.timeslots && courseSections.timeslots.map( courseInstructor => {
+                                      return (
+                                        <div key={ courseSections.crn }>
+                                          <span>Professor: </span>{ courseInstructor.instructor }
+                                          <br></br>
+                                        </div>
+                                      )
+                                    }) }
+                                    <span>CRN: </span>{ courseSections.crn }
+                                    <br></br>
+                                    <span>Seats Available: </span>{ courseSections.rem }<span>/</span>{ courseSections.cap }
+                                    <br></br>
+                                  </div>
+                                )
+                              }) }
+                              <br></br>
+                            </div>
+                          )
+                        }) }
+                      </div>
+                    )
+                  }
+                })()}
+                <div id='bottom-margin'></div>
               </div>
-              <div id='bottom-margin'></div>
-            </div>
-          )
-        } )
-      }
+            )
+          } )
+        }
+      </div>
     </div>
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,6 +20,5 @@ export default function Header({ bplLogo }) {
 function CustomLink({ id, to, children, ...props }) {
   const resolvedPath = useResolvedPath(to)
   const isActive = useMatch({ path : resolvedPath.pathname, end: true })
-
   return <Link id={id} className={isActive ? "active" : ""} to={to} {...props}>{children}</Link>
 }
