e32ec681950a40665fcddb8b57f8b6293da675ca
maguij2 <66021473+maguij2@users.noreply.github.com>
2022-11-19 17:29:02 -0500

    error handling
    added error handling for search bars

--- src/Components/ClassResults.js
+++ src/Components/ClassResults.js
@@ -2,12 +2,62 @@ import React from 'react'
 import {useLocation } from 'react-router-dom'
 import './ClassResults.css'
 
+//determine if string is only letters
+function isAlpha(s)
+{
+  return /^[a-zA-Z()]+$/.test(s);
+}
+//checks if search was valid. If the input is too long
+//or not formatted like: CSCI4440, 0 is returned
+//if the input is formatted properly but doesn't exist in catalog,
+// 1 is returned.
+//else the input is valid, return 2
+// || !(str.substring(0,4).isalpha())
+function validInput(str, dat){
+  if(str.length != 8 || !(isAlpha(str.substring(0,4))) || isNaN(str.substring(5))) {
+    return (0)
+  }
+  else if (!(dat)) {
+    return 1
+  }
+    return 2
+}
+
+//main function
 export default function ClassResults() {
   const location = useLocation();
   console.log(location)
   let rmpData = location.state.data
-  let search = location.state.professor.message
-  let courseData = location.state.courseData.CourseProfs[search.toString()]
+  let search = location.state.professor.message.toString().toUpperCase()
+  let courseData = location.state.courseData.CourseProfs[search]
+  let valid = validInput(search, courseData)
+  let numRevs = 0
+
+  // handle badly formatted user input
+  if( valid == 0){
+    return (
+      <center><div>
+        <div><span> {search} is not a properly formatted search.</span></div>
+        <div><span> Please try again with an RPI course ID; four letters followed by four numbers without spaces.</span></div>
+        <div><span> An example of a valid course ID is: 'CSCI4400' without the quotation marks. </span></div>
+        <div><span><a href="/">Click Here to Try Another Search</a></span></div>
+      </div></center>
+    )
+  }
+  // handle input of a class that doesn't exist
+  else if(valid == 1){
+    return (
+      <center><div>
+        <div><span> {search} is not a course in the RPI catalog.</span></div>
+        <div><span> Please try again with an RPI course that actually exists.</span></div>
+        <div><span> An example of a valid course ID is: 'CSCI4400' without the quotation marks. </span></div>
+        <div><span><a href="/">Click Here to Try Another Search</a></span></div>
+      </div></center>
+    )
+  }
+
+
+
   return (
     <div>
       <center><div id='course-search'>
@@ -23,6 +73,7 @@ export default function ClassResults() {
                     <div className='prof' key={ professors.profname }>
                       {(() => {
                         if (professors.profname === prof) {
+                          numRevs = numRevs + 1
                           return (
                             <div className='full-review'>
                               <div id='prof-name-class'>
@@ -40,7 +91,20 @@ export default function ClassResults() {
                                   </div>
                                 )
                               }) }
+
+
+
+
                             </div>
+
+                          )
+                        }// end of if
+                        //if there are no reviews after looking at every prof, bad input
+                        if(numRevs === 0 && (professors.profname === rmpData[rmpData.length - 1].profname)){
+                          return(<div><center>
+                                  <div><span>Unfortunately, there are no rated professors teaching this course.</span></div>
+                                  <div><span><a href="/">Click Here to Try Another Search</a></span></div>
+                                </center></div>
                           )
                         }
                       })()}
@@ -48,11 +112,12 @@ export default function ClassResults() {
                   )
                 })
               }
+
             </div>
           )
         })
       }
     </div>
-    
+
   )
 }
--- src/Components/ProfResults.js
+++ src/Components/ProfResults.js
@@ -7,6 +7,7 @@ export default function ProfResults() {
   console.log(location)
   let data = location.state.data;
   let search = location.state.professor.message;
+  let seenProf = false
   return (
     <div>
       {
@@ -15,6 +16,7 @@ export default function ProfResults() {
             <div className='prof' key={ professors.profname }>
               {(() => {
                 if (professors.profname === search) {
+                  seenProf = true
                   return (
                     <div className='full-review-prof'>
                       <div id='prof-name'>
@@ -35,6 +37,19 @@ export default function ProfResults() {
                     </div>
                   )
                 }
+                // Handle invalid user input. Tell user that their input was wrong, then provide a link
+                // to the professor list page, and a link back to the home page
+                else if(seenProf === false && (professors.profname === data[data.length - 1].profname)){
+                  return (
+                    <div>
+                      <div><span>{search} is not an RPI professor that has any ratings.</span></div>
+                      <div><span><a href="/profhome">Click Here to See a List of RPI Professors with Ratings</a></span></div>
+                      <div><span><a href="/">Click Here to Return to Search</a></span></div>
+                    </div>
+
+                  )
+                }
+
               })()}
             </div>
           )
