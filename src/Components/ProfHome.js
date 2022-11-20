b4b7e2f1b80e97454e75c2ffa6df3d64f636f657
Yotham <yothamsage@gmail.com>
2022-11-20 13:37:59 -0500

    sorted page

--- src/Components/ProfHome.js
+++ src/Components/ProfHome.js
@@ -5,23 +5,34 @@ export default function ProfHome({ data }) {
   return (
     <div className='prof-list'>
       {
-        data && data.map( profName => {
+        data && data
+        .sort((a,b) => a.overall_rating < b.overall_rating ? 1 : -1)
+        .map( profName => {
           return (
-            <div className='prof-name' key={ profName.profName }>
-              { profName.profname } --- <span> </span>
-              { profName.overall_rating.toFixed(1)}
-              <br></br><br></br>
-              { profName.reviews && profName.reviews.map( ratings => {
-                return (
-                  <div key={ ratings.className }>
-                    { ratings.className } <br></br>
-                    { ratings.reviewEmotion } <br></br>
-                    { ratings.qualityRating.toFixed(1)} <br></br>
-                    { ratings.review } <br></br><br></br>
-                  </div>
-                )
-              }) }
-            </div>
+          <div>
+          {(() => {
+            if(profName.overall_rating != 0.0){
+              return(
+                <div className='prof-name' key={ profName.profName }>
+                { profName.profname } --- <span> </span>
+                { profName.overall_rating.toFixed(1)}
+                <br></br><br></br>
+                { profName.reviews && profName.reviews.map( ratings => {
+                  return (
+                    <div key={ ratings.className }>
+                      { ratings.className } <br></br>
+                      { ratings.reviewEmotion } <br></br>
+                      { ratings.qualityRating.toFixed(1) } <br></br>
+                      { ratings.review } <br></br><br></br>
+                    </div>
+                  )
+                }) }
+                
+              </div>
+              )
+            }
+          })()}
+          </div>
           )
         })
       }
