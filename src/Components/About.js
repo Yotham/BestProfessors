6f57ca78026fb9eb01f0b4f17a3f7f4a9da3fa1b
Yotham <yothamsage@gmail.com>
2022-11-21 16:26:08 -0500

    Updated about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,34 @@
 import React from 'react'
-
+import './About.css'
 export default function About() {
   return (
     <div>
-        <div className="about-title">
-          About Us:
+        <div id = "about-title" className="about-title">
+          About Us
         </div>
-        <div className="about-p">
-          Here is a paragraph about us. We are a group that is working on this project for this class that we are taking. The project is this website. Blah, blah, blah. Also here are some pictures of our group below:
-        </div>
+        <center><div class="row">
+          <div class = "column">
+              <img id = "member_photo" alt = "Yotham Sage" src = "https://i.ibb.co/8YCb791/yotham-sage.jpg" ></img>
+              <p>Yotham Sage</p>
+          </div>
+          <div class = "column">
+              <img id = "member_photo" alt = "Jen Lawless" src = "https://i.ibb.co/7RPGXb0/jenny-lawless.jpg" ></img>
+              <p>Jen Lawless</p>
+          </div>
+          <div class = "column">
+              <img id = "member_photo" src = "https://i.ibb.co/8YCb791/yotham-sage.jpg" ></img>
+              <p>Isaac Foster</p>
+          </div>
+          <div class = "column">
+              <img id = "member_photo" src = "https://i.ibb.co/8YCb791/yotham-sage.jpg" ></img>
+              <p>Julia Maguire</p>
+          </div>
+          <div class = "column">
+              <img id = "member_photo" src = "https://i.ibb.co/8YCb791/yotham-sage.jpg" ></img>
+              <p>Teddy Clark</p>
+          </div>
+        </div></center>
+        <center><div id = "paragraph-about-us">A group of RPI students dedicated to making the class selection process easier, our mission was to eliminate the need to sift through RateMyProfessor to find professor ratings and instead allow students to enter their course code and receive all the necessary information.</div></center>
     </div>
   )
 }
