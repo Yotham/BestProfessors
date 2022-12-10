a49e23bff015ad9675fb58e0b505d1a7163c3205
Yotham <yothamsage@gmail.com>
2022-12-09 21:18:43 -0500

    linted test files

--- src/Components/About.test.js
+++ src/Components/About.test.js
@@ -1,36 +1,44 @@
+/* eslint-disable linebreak-style */
+/* eslint max-len: ["error", { "code": 150 }] */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+/* eslint camelcase: "error" */
+/* eslint indent: ["error", 2] */
 import React from 'react';
-import { render } from '@testing-library/react';
-import About from './About'
+import {render} from '@testing-library/react';
+import About from './About';
 
 describe(About, () => {
-    it("About displays correct information", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("paragraph-test").textContent;
-        expect(pageValue).toEqual("A group of RPI students dedicated to making the class selection process easier, our mission was to eliminate the need to sift through RateMyProfessor to find professor ratings and instead allow students to enter their course code and receive all the necessary information.")
-    });
-    it("About displays correct name", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("name1").textContent;
-        expect(pageValue).toEqual("Yotham Sage")
-    });
-    it("About displays correct name", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("name2").textContent;
-        expect(pageValue).toEqual("Jen Lawless")
-    });
-    it("About displays correct name", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("name3").textContent;
-        expect(pageValue).toEqual("Isaac Foster")
-    });
-    it("About displays correct name", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("name4").textContent;
-        expect(pageValue).toEqual("Julia Maguire")
-    });
-    it("About displays correct name", () => {
-        const { getByTestId } = render(<About />);
-        const pageValue = getByTestId("name5").textContent;
-        expect(pageValue).toEqual("Teddy Clark")
-    });
-})
+  it('About displays correct information', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('paragraph-test').textContent;
+    expect(pageValue).toEqual('A group of RPI students dedicated to making the class selection process easier,'+
+     ' our mission was to eliminate the need to sift through RateMyProfessor to find professor ratings and' +
+     'instead allow students to enter their course code and receive all the necessary information.');
+  });
+  it('About displays correct name', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('name1').textContent;
+    expect(pageValue).toEqual('Yotham Sage');
+  });
+  it('About displays correct name', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('name2').textContent;
+    expect(pageValue).toEqual('Jen Lawless');
+  });
+  it('About displays correct name', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('name3').textContent;
+    expect(pageValue).toEqual('Isaac Foster');
+  });
+  it('About displays correct name', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('name4').textContent;
+    expect(pageValue).toEqual('Julia Maguire');
+  });
+  it('About displays correct name', () => {
+    const {getByTestId} = render(<About />);
+    const pageValue = getByTestId('name5').textContent;
+    expect(pageValue).toEqual('Teddy Clark');
+  });
+});
--- src/Components/Contact.test.js
+++ src/Components/Contact.test.js
@@ -1,21 +1,27 @@
+/* eslint-disable linebreak-style */
+/* eslint max-len: ["error", { "code": 150 }] */
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+/* eslint camelcase: "error" */
+/* eslint indent: ["error", 2] */
 import React from 'react';
-import { render } from '@testing-library/react';
-import Contact from './Contact'
+import {render} from '@testing-library/react';
+import Contact from './Contact';
 
 describe(Contact, () => {
-    it("Contact displays correct information", () => {
-        const { getByTestId } = render(<Contact />);
-        const pageValue = getByTestId("title-test").textContent;
-        expect(pageValue).toEqual("Contact:")
-    });
-    it("Contact displays correct information", () => {
-        const { getByTestId } = render(<Contact />);
-        const pageValue = getByTestId("p1-test").textContent;
-        expect(pageValue).toEqual("Let us know if you have any suggestions or issues!")
-    });
-    it("Contact displays correct information", () => {
-        const { getByTestId } = render(<Contact />);
-        const pageValue = getByTestId("p2-test").textContent;
-        expect(pageValue).toEqual("Email: bestprofessorlist@gmail.com")
-    });
-})
+  it('Contact displays correct information', () => {
+    const {getByTestId} = render(<Contact />);
+    const pageValue = getByTestId('title-test').textContent;
+    expect(pageValue).toEqual('Contact:');
+  });
+  it('Contact displays correct information', () => {
+    const {getByTestId} = render(<Contact />);
+    const pageValue = getByTestId('p1-test').textContent;
+    expect(pageValue).toEqual('Let us know if you have any suggestions or issues!');
+  });
+  it('Contact displays correct information', () => {
+    const {getByTestId} = render(<Contact />);
+    const pageValue = getByTestId('p2-test').textContent;
+    expect(pageValue).toEqual('Email: bestprofessorlist@gmail.com');
+  });
+});
